10cb45d7bbeefa8b2b7e36c869e1167961cd97f8
kajate <patrick.gardinger@gmail.com>
2018-06-05 16:40:27 +0200

    added imageboard

---
+++ Files and Documents/Summarytext.md
@@ -0,0 +1,5 @@
+I am a designer and developer looking for oppertunities to build interactive solutions that engages users and creates purpose in both function and look.
+
+With experience in various fields, including Interaction Design, Team Dynamics and Digital Strategies,  I have a toolbox filled with possibilites to contribute with, on several levels. 
+
+I want to be active in an environment where I work closely with people that inspires to take initative and responsibility, creating excellent solutions and produtcs that builds on creative freedom and communicative team work.
--- server.js
+++ server.js
@@ -14,7 +14,29 @@ app.get("/", (req, res) =>{
     res.render("home", {
         project: projects,
         link: links,
-        layout: "main"
+        layout: "main",
+    });
+});
+
+app.get("/projects/:name/public/about", (req, res) =>{
+    var projects = fs.readdirSync(__dirname + "/public/projects");
+    var links = __dirname + "/public/projects/";
+    var obj = require(links + req.params.name + "/public/about.json");
+
+    res.render("about", {
+        layout: "main",
+        name: obj.name,
+        project: projects,
+        link: links,
+        url: req.url.replace("description", ""),
+        imgSrc: obj.image,
+        title: obj.name,
+        desc: obj.description,
+        desctwo: obj.descriptiontwo,
+        descthree: obj.descriptionthree,
+        techs: obj.techs,
+        code: obj.code,
+        file: obj.file
     });
 });
 
@@ -22,10 +44,10 @@ app.get("/projects/:name/public/description", (req, res) => {
     // res.send(`<h1>${req.params.name}</h1>`);
     var projects = fs.readdirSync(__dirname + "/public/projects");
     var links = __dirname + "/public/projects/";
-    // var src = req.params.name + ".png";
+    // var file = req.params.name + ".pdf";
     var obj = require(links + req.params.name + "/public/description.json");
     res.render("other", {
-        name: req.params.name,
+        name: obj.name,
         project: projects,
         link: links,
         url: req.url.replace("description", ""),
@@ -33,8 +55,11 @@ app.get("/projects/:name/public/description", (req, res) => {
         imgSrc: obj.image,
         title: obj.name,
         desc: obj.description,
+        desctwo: obj.descriptiontwo,
+        descthree: obj.descriptionthree,
         techs: obj.techs,
-        code: obj.code
+        code: obj.code,
+        file: obj.file
     });
 });
 
