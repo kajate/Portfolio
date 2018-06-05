e90b4e65396b05756822a03a3697b95a271d4147
kajate <patrick.gardinger@gmail.com>
2018-06-06 00:40:08 +0200

    changes

--- server.js
+++ server.js
@@ -18,33 +18,9 @@ app.get("/", (req, res) =>{
     });
 });
 
-app.get("/projects/:name/public/about", (req, res) =>{
-    var projects = fs.readdirSync(__dirname + "/public/projects");
-    var links = __dirname + "/public/projects/";
-    var obj = require(links + req.params.name + "/public/about.json");
-
-    res.render("about", {
-        layout: "main",
-        name: obj.name,
-        project: projects,
-        link: links,
-        url: req.url.replace("description", ""),
-        imgSrc: obj.image,
-        title: obj.name,
-        desc: obj.description,
-        desctwo: obj.descriptiontwo,
-        descthree: obj.descriptionthree,
-        techs: obj.techs,
-        code: obj.code,
-        file: obj.file
-    });
-});
-
 app.get("/projects/:name/public/description", (req, res) => {
-    // res.send(`<h1>${req.params.name}</h1>`);
     var projects = fs.readdirSync(__dirname + "/public/projects");
     var links = __dirname + "/public/projects/";
-    // var file = req.params.name + ".pdf";
     var obj = require(links + req.params.name + "/public/description.json");
     res.render("other", {
         name: obj.name,
